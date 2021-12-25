d23416ed4e191a318b67cd52e91f0924596a6154
Ihor-Hoshko <fata77698@gmail.com>
2021-12-25 19:55:22 +0200

    1 begin

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,5 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const buttonStart = document.querySelector('button[data - start]');
+const buttonStop = document.querySelector('button[data - stop]');
