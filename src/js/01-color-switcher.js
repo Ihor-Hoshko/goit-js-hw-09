092b0eec746132504736a7b33e211009d4312dc7
Ihor-Hoshko <fata77698@gmail.com>
2021-12-26 15:43:02 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const buttonStart = document.querySelector('button[data - start]');
-const buttonStop = document.querySelector('button[data - stop]');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+timerId = null;
+buttonStart.addEventListener('click', colorSwitch);
+buttonStop.addEventListener('click', colorSwitchStop);
+
+function colorSwitch() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+}
+
+function colorSwitchStop() {
+  if (timerId) {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+  }
+}
